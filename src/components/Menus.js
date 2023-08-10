c680ae70921ee9cbd5a66526be58526c29e310a3
Audrey Axel Ristyanto <audrey.axel@suitmedia.com>
2023-08-11 01:23:54 +0700

    Added dashboard elements and styling

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -2,7 +2,19 @@ import "@/styles/menus.css";
 import "@/app/globals.css";
 
 const Menus = () => {
-  return <>Menus</>;
+  return (
+    <div className="hidden md:block fixed h-full bg-neutral drop-shadow-lg rounded-r-3xl px-4 pt-4">
+      <h1 className="dashboard">Dashboard</h1>
+      <ul className="menus-ul">
+        <li>
+          <button>Products</button>
+        </li>
+        <li>
+          <button>Carts</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Menus;
